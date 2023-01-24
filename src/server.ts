4ec8c3852fe8c1c6cd38c82fb82ddceffbbc1191
William Roger <williamroger.frontend@gmail.com>
2023-01-24 17:34:55 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  response.json({ message: 'Hello Friends!'});
+});
+
+app.listen(3333, () => console.log('Server is running...'));
