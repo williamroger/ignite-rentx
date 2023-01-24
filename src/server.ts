412c9d0256d32f03fb094d73cac2e23c8a26ab27
William Roger <williamroger.frontend@gmail.com>
2023-01-24 18:23:57 -0300

    iniciada a rota /categories

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
+import { categoriesRoutes } from './routes/categories.routes';
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  response.json({ message: 'Hello Friends!'});
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log('Server is running...'));
