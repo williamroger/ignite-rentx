c5b20c6862f0190361c6fd77c21f4bf195fe82e3
William Roger <williamroger.frontend@gmail.com>
2023-01-26 21:22:48 -0300

    adicionado validação para cadastrar categorias

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -27,6 +27,14 @@ class CategoriesRepository {
   list(): Category[] {
     return this.categories;
   }
+
+  findByName(name: string): Category {
+    const category = this.categories.find(category => 
+      category.name.toLowerCase() === name.toLowerCase()
+    );
+
+    return category;
+  }
 }
 
 export { CategoriesRepository };
