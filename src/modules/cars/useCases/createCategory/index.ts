b551d4d20f0a77c1427c086133d4a50a3a0a367c
William Roger <williamroger.frontend@gmail.com>
2023-02-11 22:52:47 -0300

    aplicando Singleton Pattern na aplicação

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -2,7 +2,7 @@ import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 import { CategoriesRepository } from '../../repositories/CategoriesRepository';
 import { CreateCategoryController } from "./CreateCategoryController";
 
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = CategoriesRepository.getInstance();
 
 const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
 
--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -2,7 +2,7 @@ import { ListCategoriesController } from "./ListCategoriesController";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 import { CategoriesRepository } from '../../repositories/CategoriesRepository';
 
-const categoriesRespository = new CategoriesRepository();
+const categoriesRespository = CategoriesRepository.getInstance();
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRespository);
 const listCategoriesController = new ListCategoriesController(listCategoriesUseCase);
 
