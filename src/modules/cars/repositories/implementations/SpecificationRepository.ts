e4818bd9336cd613f0f925e2cd0fa2ce54048300
William Roger <williamroger.frontend@gmail.com>
2023-05-04 01:17:41 -0300

    refatorando o model de categoria

--- src/modules/cars/repositories/implementations/SpecificationRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationRepository.ts
@@ -1,4 +1,4 @@
-import { Specification } from "../../models/Specification";
+import { Specification } from "../../entities/Specification";
 import { ICreateSpecificationDTO, ISpecificationRepository } from "../ISpecificationRepository";
 
 class SpecificationRepository implements ISpecificationRepository {
