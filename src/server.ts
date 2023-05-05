90945ae03747e8d155d5532bf80b3227fc0f9d06
William Roger <williamroger.frontend@gmail.com>
2023-05-05 18:45:32 -0300

    aplicando injeção de dependência com tsyringe

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 
-import swaggerFile from './swagger.json';
 import './database';
+import './shared/container';
+
+// import routes here
+import swaggerFile from './swagger.json';
 
 import { categoriesRoutes } from './routes/categories.routes';
 import { specificationsRouter } from './routes/specification.routes';
