d23bbe8af0aebbab0c963ca761a9e9807b5a019e
William Roger <williamroger.frontend@gmail.com>
2023-05-05 13:11:55 -0300

    refatorando caso de uso de categoria

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -2,7 +2,7 @@ import { ListCategoriesController } from "./ListCategoriesController";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
 
-const categoriesRespository = CategoriesRepository.getInstance();
+const categoriesRespository = null;
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRespository);
 const listCategoriesController = new ListCategoriesController(listCategoriesUseCase);
 
