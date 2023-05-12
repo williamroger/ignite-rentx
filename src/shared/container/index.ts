4f5d975b93eaa7c3e52c07485a29bc9ab4a9d648
William Roger <williamroger.frontend@gmail.com>
2023-05-12 17:58:43 -0300

    adicionado repositório de usuários

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,9 +2,11 @@ import { container } from 'tsyringe';
 
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationRepository } from '../../modules/cars/repositories/ISpecificationRepository';
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
 import { SpecificationRepository } from '../../modules/cars/repositories/implementations/SpecificationRepository';
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
@@ -14,4 +16,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationRepository>(
   'SpecificationRepository',
   SpecificationRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository
 );
